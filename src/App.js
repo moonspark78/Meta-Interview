bb4c75e446ab97a34ece7df344bf1782b043bc0f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-27 14:14:07 +0200

    :construction:  Work in Progress :construction:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Actions } from './Actions';
 import { List } from './List';
 import './App.css';
+import {items} from "./data"
+import { useState } from 'react';
 
 function App() {
+  const [leftItems, setLeftItems] = useState(items)
+  const [righrItems, setRightItems] = useState([])
   return (
     <div className="App flex">
      <List/>
